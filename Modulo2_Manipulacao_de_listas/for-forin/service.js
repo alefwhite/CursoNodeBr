9fbfcb16888614fc54fd2c8afa405e14db5c72e0
alefwhite <alefwhite@hotmail.com>
2020-01-13 18:04:01 -0300

    "Terminando moduloe de listas"

--- Modulo2_Manipulacao_de_listas/for-forin/service.js
+++ Modulo2_Manipulacao_de_listas/for-forin/service.js
@@ -1,3 +1,12 @@
-// npm install axios
-const axios = require('axios');
-const URL = 'https://swapi.co/api/people';
+const axios = require('axios')
+const URL = `https://swapi.co/api/people`
+
+async function obterPessoas(nome) {
+    const url = `${URL}/?search=${nome}&format=json`
+    const response = await axios.get(url)
+    return response.data
+}
+
+module.exports = {
+    obterPessoas
+}
